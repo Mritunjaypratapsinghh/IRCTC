b2932670291709b54b803fef5e77c72a01b55566
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-03 13:27:44 +0530

    Getter and Setter of Train Entity

--- app/src/main/java/ticket/booking/entities/Train.java
+++ app/src/main/java/ticket/booking/entities/Train.java
@@ -1,6 +1,5 @@
 package ticket.booking.entities;
 
-import java.sql.Time;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +9,60 @@ public class Train {
     private String trainNo;
     private Date departTime;
     private Date arrivalTime;
-    private List<List<Boolean>> seats;
-    private Map<String, Time> stationTime;
+    private List<List<Integer>> seats;
+    private Map<String, String> stationTime;
     private List<String> stations;
+
+    public Train(){}
+
+
+    public Train(String trainId, String trainNo,  List<List<Integer>> seats, Map<String,String> stationTime, List<String> stations){
+        this.trainId = trainId;
+        this.trainNo = trainNo;
+        this.seats = seats;
+        this.stationTime = stationTime;
+        this.stations = stations;
+    }
+
+
+    public List<String> getStations(){
+        return this.getStations();
+    }
+
+    public List<List<Integer>> getSeats(){
+        return this.seats;
+    }
+
+    public String getTrainId(){
+        return trainId;
+    }
+
+    public Map<String,String> getStationTime(){
+        return stationTime;
+    }
+
+    public String getTrainNo(){
+        return trainNo;
+    }
+
+    public void setTrainId(String trainId){
+        this.trainId = trainId;
+
+    }
+
+    public void setStationTime(Map<String,String> stationTime){
+        this.stationTime = stationTime;
+    }
+
+    public void setStations(List<String> stations){
+        this.stations = stations;
+    }
+
+    public String getTrainInfo(){
+        return String.format("Train ID: %s Train No: %s", trainId,trainNo);
+    }
+
+
 
 
 }
