aa337face52e4e9817bef52fe8067fb56afea64d
Mritunjay Pratap Singh <mritunjaypratapsinghh@gmail.com>
2025-03-03 13:28:20 +0530

    Getter and Setter of Ticket Entity

--- app/src/main/java/ticket/booking/entities/Ticket.java
+++ app/src/main/java/ticket/booking/entities/Ticket.java
@@ -8,7 +8,75 @@ public class Ticket {
     private String userId;
     private String source;
     private String destination;
-    private Date dateOfTravel;
+    private String dateOfTravel;
     private List<Train> train;
+
+    public Ticket(){}
+
+    public Ticket(String tickedId, String userId, String source, String destination, String dateOfTravel,List<Train> train){
+        this.tickedId = tickedId;
+        this.userId = userId;
+        this.source = source;
+        this.destination = destination;
+        this.dateOfTravel = dateOfTravel;
+        this.train = train;
+    }
+
+    public String getTicketInfo(){
+        return String.format("Ticket ID: %s belongs to User %s from %s to %s on %s", tickedId, userId, source, destination, dateOfTravel);
+    }
+
+
+
+    public String getTickedId(){
+        return this.tickedId;
+    }
+
+    public void setTicketId(String tickedId){
+        this.tickedId = tickedId;
+    }
+
+    public String getSource(){
+        return this.source;
+    }
+
+    public void setSource(String source){
+        this.source = source;
+    }
+
+    public String getDestination(){
+        return this.destination;
+    }
+
+    public void setDestination(){
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel(){
+        return this.dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel){
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public String getUserId(){
+        return this.userId;
+    }
+
+    public void setUserId(){
+        this.userId = userId;
+    }
+
+    public List<Train> getTrain(){
+        return this.train;
+    }
+
+    public void setTrain(List<Train> train){
+        this.train = train;
+    }
+
+
+
 
 }
